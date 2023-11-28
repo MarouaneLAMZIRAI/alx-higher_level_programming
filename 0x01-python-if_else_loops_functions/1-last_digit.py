82f26b048f48a3aaba4c0ea1f0d39cd117efa7ee
Your Name <you@example.com>
2023-11-28 02:17:37 -0800

    second comm

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ last_digit = int(str(number)[-1])
 to_be_printed = "Last digit of "+ str(number) + " is "+ str(last_digit)
 
 if last_digit > 5 :
-    to_be_printed = to_be_printed + " and is greater than 5 "
+    to_be_printed = to_be_printed + " and is greater than 5"
 elif last_digit == 0 :
     to_be_printed = to_be_printed + " and is 0"
 elif last_digit < 6 :
