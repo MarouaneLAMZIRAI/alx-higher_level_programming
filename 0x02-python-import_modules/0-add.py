b29cda95c1b59b151a63c18030b8224dd237cf42
Your Name <you@example.com>
2023-11-30 14:05:28 -0800

    first comm

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-
-# Importing the function 'add' from add_0.py
-from add_0 import add
-
-# Assigning values to variables a and b
-a = 1
-b = 2
-
-# Calling the add function and printing the result using string formatting
-print("{} + {} = {}".format(a, b, add(a, b)))
-
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
