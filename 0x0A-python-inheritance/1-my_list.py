a93380e4ab66409f63f9f8b0e645b7c2a1073fa6
Your Name <you@example.com>
2024-01-08 13:53:10 -0800

    Commit all the project

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+class Mylist
+"""
+
+
+class MyList(list):
+    """MyList class
+    """
+
+    def print_sorted(self):
+        """print list
+        """
+        print(sorted(self))
+
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+this is for ALX Python inheritance project
